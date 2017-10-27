68cf0b97ab65f66bbd54d4944a7e6967c19316e0
Unknown <raquelpa93@gmail.com>
2017-10-27 13:19:13 +0200

    me hago un simple reading para testear cosas

--- Code/simple_reading.py
+++ Code/simple_reading.py
@@ -1,18 +1,33 @@
-import sys
-sys.modules[__name__].__dict__.clear()
-
-
-import numpy as np
-import matplotlib.pyplot as plt
-from wordnet_imagenet_connections import Statistics
+"""
+El objetivo de este programa es tener código vacío para testear funciones a parte
+"""
+from wordnet_imagenet_connections import Data, Statistics
 from nltk.corpus import wordnet as wn
-
 
 dog = wn.synsets('dog')[0]
 mammal = wn.synsets('mammal')[0]
 living_things = wn.synset('living_thing.n.01')
 hunting_dogs = wn.synsets('hunting_dog')[0]
 
-synsets = [living_things, mammal, dog, hunting_dogs]
+synsets_living = [living_things, mammal, dog, hunting_dogs]
+minisyn = [hunting_dogs]
 
-stats = Statistics(synsets)
+object = wn.synsets('artifact')[0]
+instrum = wn.synset('instrumentality.n.03')
+conv = wn.synset('conveyance.n.03')
+wheeled_vehicle = wn.synsets('wheeled_vehicle')[0]
+
+synsets_non_living = [object, instrum, conv, wheeled_vehicle]
+
+embeddings_version = [25]
+for version in embeddings_version:
+
+    data = Data(version)
+    stats_living = Statistics(synsets_living, data)
+    stats_non_living = Statistics(synsets_non_living, data)
+
+    for synset in synsets_living:
+        stats_living.plot_images_per_feature_of_synset_per_layer(synset)
+
+    for synset in synsets_non_living:
+        stats_non_living.plot_images_per_feature_of_synset_per_layer(synset)
